8878a7e811ef4dc002446261247970a57f7f6372
sander <sander.philipsen@hotmail.com>
2018-12-17 23:45:06 +0100

    Auditief bombardement

--- app/src/main/java/be/thomasmore/project_mobile_dev/LuisterGoed.java
+++ app/src/main/java/be/thomasmore/project_mobile_dev/LuisterGoed.java
@@ -1,22 +1,22 @@
 package be.thomasmore.project_mobile_dev;
-
+import java.util.Random;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.widget.ImageView;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.media.MediaPlayer;
 import be.thomasmore.project_mobile_dev.classes.Spel;
-import be.thomasmore.project_mobile_dev.classes.SpelType;
 
 public class LuisterGoed extends AppCompatActivity {
     public Spel spel;
+    public MediaPlayer player;
+    public MediaPlayer player2;
     private DatabaseHelper db;
     public long spelId;
+    Random rand ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +26,111 @@ public class LuisterGoed extends AppCompatActivity {
         db = new DatabaseHelper(this);
         spel = new Spel();
         Bundle bundle = getIntent().getExtras();
-
-        spel.setDoelklankId( bundle.getLong("doelklankId"));
+        spel.setDoelklankId(bundle.getLong("doelklankId"));
         spel.setGebruikerId(bundle.getLong("gebruikerId"));
         spel.setSpeltypeId(bundle.getLong("speltypeId"));
         spelId = db.insertSpel(spel);
-
-            }
     }
+    public void speel_onClick(View v) {
+        player = null;
+        if (player == null) {
+            switch (Integer.parseInt(String.valueOf(spel.getDoelklankId()))) {
+                case 1:
+                    player = MediaPlayer.create(this, R.raw.reeks1);
+                    break;
+                case 2:
+                    player = MediaPlayer.create(this, R.raw.reeks5);
+                    break;
+                case 3:
+                    player = MediaPlayer.create(this, R.raw.reeks3);
+                    break;
+                case 4:
+                    player = MediaPlayer.create(this, R.raw.reeks2);
+                    break;
+                case 5:
+                    player = MediaPlayer.create(this, R.raw.reeks4);
+                    break;
+                case 6:
+                    player = MediaPlayer.create(this, R.raw.reeks6);
+                    break;
+                case 7:
+                    player = MediaPlayer.create(this, R.raw.reeks3);
+                    break;
+                case 8:
+                    player = MediaPlayer.create(this, R.raw.reeks4);
+                    break;
+                case 9:
+                    int randomNum = rand.nextInt((1 - 0) + 1);
+                    if (randomNum == 1) {
+                        player = MediaPlayer.create(this, R.raw.reeks7);
+                    }
+                    else {
+                        player = MediaPlayer.create(this, R.raw.reeks8);
+                    }
+                    break;
+                case 10:
+                    player = MediaPlayer.create(this, R.raw.reeks9);
+            }
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                goedGedaan();
+                }
+            });
+            player.start();
+        }
+    }
+    public void Stop_onClick(View v)
+    {
+        stopPlayer();
+    }
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+
+        }
+        if (player2 != null) {
+            player2.release();
+            player2 = null;
+
+        }
+    }
+    private void goedGedaan() {
+        final ImageView imageView = (ImageView) findViewById(R.id.oor);
+        imageView.setImageResource(R.drawable.minimedaille1);
+        if (player != null) {
+            player = MediaPlayer.create(this, R.raw.goed_gedaan);
+            player.start();
+            player2 = MediaPlayer.create(this, R.raw.applaus);
+            player2.start();
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                   terug_Naar_Home();
+                }
+
+            });
+        }
+    }
+    public void Pause_onClick(View v) {
+        if (player != null) {
+            player.pause();
+        }
+    }
+    public void terug_Naar_Home() {
+        Bundle bundle = new Bundle();
+        bundle.putLong("gebruikerId", spel.getGebruikerId());
+        Intent intent = new Intent(this, Home.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+}
 
 
